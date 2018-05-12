dc983eb7b46705b8fcdc3104b2c878e460041d98
schneiderklaus <klaus@cs.arizona.edu>
2018-05-12 17:18:33 -0400

    pushed ndn consumer-pcon

---
+++ apps/ndn-consumer-pcon.cpp
@@ -0,0 +1,65 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (c) 2011-2015  Regents of the University of California.
+ *
+ * This file is part of ndnSIM. See AUTHORS for complete list of ndnSIM authors and
+ * contributors.
+ *
+ * ndnSIM is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * ndnSIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#include "ndn-consumer-pcon.hpp"
+//#include "ns3/ptr.h"
+//#include "ns3/log.h"
+//#include "ns3/simulator.h"
+//#include "ns3/packet.h"
+//#include "ns3/callback.h"
+//#include "ns3/string.h"
+//#include "ns3/uinteger.h"
+//#include "ns3/double.h"
+
+NS_LOG_COMPONENT_DEFINE("ndn.ConsumerPCON");
+
+namespace ns3 {
+namespace ndn {
+
+
+NS_OBJECT_ENSURE_REGISTERED(ConsumerPCON);
+
+TypeId
+ConsumerPCON::GetTypeId(void)
+{
+  static TypeId tid =
+    TypeId("ns3::ndn::ConsumerPCON")
+      .SetGroupName("Ndn")
+      .SetParent<ConsumerWindow>()
+      .AddConstructor<ConsumerPCON>();
+
+  return tid;
+}
+
+
+void
+ConsumerPCON::OnData(shared_ptr<const Data> contentObject)
+{
+  auto congMark = contentObject->getCongestionMark();
+  if (congMark > 0) {
+    std::cout << "Consumer got congestion mark!\n";
+  }
+
+  ConsumerWindow::OnData(contentObject);
+}
+
+
+
+} // namespace ndn
+} // namespace ns3
---
+++ apps/ndn-consumer-pcon.hpp
@@ -0,0 +1,66 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (c) 2011-2015  Regents of the University of California.
+ *
+ * This file is part of ndnSIM. See AUTHORS for complete list of ndnSIM authors and
+ * contributors.
+ *
+ * ndnSIM is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * ndnSIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#ifndef NDN_CONSUMER_PCON_H
+#define NDN_CONSUMER_PCON_H
+
+#include "ndn-consumer-window.hpp"
+
+namespace ns3 {
+namespace ndn {
+
+/**
+ * @ingroup ndn-apps
+ * \brief Ndn application for sending out Interest packets (window-based)
+ *
+ * !!! ATTENTION !!! This is highly experimental and relies on experimental features of the
+ *simulator.
+ * Behavior may be unpredictable if used incorrectly.
+ */
+class ConsumerPCON : public ConsumerWindow {
+public:
+  static TypeId
+  GetTypeId();
+
+  /**
+   * \brief Default constructor
+   */
+  ConsumerPCON() {
+    ConsumerWindow();
+  }
+
+  // From App
+  virtual void
+  OnData(shared_ptr<const Data> contentObject) override;
+
+//  virtual void
+//  OnTimeout(uint32_t sequenceNumber);
+//
+//  virtual void
+//  WillSendOutInterest(uint32_t sequenceNumber);
+
+public:
+  typedef void (*WindowTraceCallback)(uint32_t);
+
+};
+
+} // namespace ndn
+} // namespace ns3
+
+#endif
